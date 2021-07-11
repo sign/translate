9ad06b1890d5e233b794f97380e28219367a267f
Amit <amitmoryossef@gmail.com>
2021-07-11 11:37:26 +0300

    fix?(pages): add 404 page

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,9 @@
 import {enableProdMode} from '@angular/core';
 import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 import {defineCustomElements as defineCustomElementsSW} from '@sutton-signwriting/sgnw-components/loader';
-import {defineCustomElements as defineCustomElementsPoseViewer} from 'pose-viewer/loader';
-
 import {AppModule} from './app/app.module';
 import {environment} from './environments/environment';
+// import {defineCustomElements as defineCustomElementsPoseViewer} from 'pose-viewer/loader';
 
 if (environment.production) {
   enableProdMode();
@@ -14,4 +13,4 @@ platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
 
 defineCustomElementsSW();
-defineCustomElementsPoseViewer();
+// defineCustomElementsPoseViewer();
