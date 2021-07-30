0ab5e5bbce4de1d7b3e1c4113f85101d19c6e866
Amit <amitmoryossef@gmail.com>
2021-07-30 13:06:31 +0300

    test(signwriting): test wrapper make sure color changes

--- src/app/pages/translate/signwriting/sign-writing.component.spec.ts
+++ src/app/pages/translate/signwriting/sign-writing.component.spec.ts
@@ -2,10 +2,17 @@ import {ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {SignWritingComponent} from './sign-writing.component';
 import {CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
+import {defineCustomElements as defineCustomElementsSW} from '@sutton-signwriting/sgnw-components/loader';
+
 
 describe('SignWritingComponent', () => {
   let component: SignWritingComponent;
   let fixture: ComponentFixture<SignWritingComponent>;
+
+  beforeAll(async () => {
+    await defineCustomElementsSW();
+    await customElements.whenDefined('sgnw-sign');
+  });
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -17,10 +24,58 @@ describe('SignWritingComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(SignWritingComponent);
     component = fixture.componentInstance;
+    component.signs = ['M507x523S15a28494x496S26500493x477'];
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should change color when prefers-color-scheme changes', async () => {
+    await customElements.whenDefined('sgnw-sign');
+
+    const getColor = async () => {
+      const getSVG = () => {
+        const el = fixture.nativeElement.querySelector('sgnw-sign');
+        if (!el) {
+          return null;
+        }
+        const slot = el.shadowRoot.querySelector('slot');
+        if (!slot) {
+          return null;
+        }
+        return slot.assignedElements()[0];
+      };
+
+      let svg;
+      while (!svg) {
+        fixture.detectChanges();
+        await new Promise(resolve => setTimeout(resolve, 10)); // Wait for element to be fully loaded
+        svg = getSVG();
+      }
+
+      const textEl = svg.querySelector('text.sym-line');
+
+      return textEl.getAttribute('fill');
+    };
+
+    const specialColor = '#012345';
+
+    const currentColor = await getColor();
+    expect(currentColor).not.toBe(specialColor);
+
+    // Change element color
+    fixture.nativeElement.style.color = specialColor;
+    component.colorSchemeMedia.dispatchEvent(new Event('change'));
+
+    const newColor = await getColor();
+    expect(newColor).toBe(specialColor);
+  });
+
+  it('should unregister event listenr on destroy', () => {
+    const spy = spyOn(component.colorSchemeMedia, 'removeEventListener');
+    component.ngOnDestroy();
+    expect(spy).toHaveBeenCalled();
   });
 });
--- src/app/pages/translate/spoken-to-signed/spoken-to-signed.component.spec.ts
+++ src/app/pages/translate/spoken-to-signed/spoken-to-signed.component.spec.ts
@@ -1,18 +1,20 @@
-import {ComponentFixture, TestBed} from '@angular/core/testing';
+import {ComponentFixture, fakeAsync, TestBed, tick} from '@angular/core/testing';
 
 import {SpokenToSignedComponent} from './spoken-to-signed.component';
 import {SignWritingComponent} from '../signwriting/sign-writing.component';
 import {TextToSpeechComponent} from '../../../components/text-to-speech/text-to-speech.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
-import {NgxsModule} from '@ngxs/store';
+import {NgxsModule, Store} from '@ngxs/store';
 import {SettingsState} from '../../../modules/settings/settings.state';
 import {ngxsConfig} from '../../../core/modules/ngxs/ngxs.module';
 import {TranslateState} from '../../../modules/translate/translate.state';
+import {SetSpokenLanguageText} from '../../../modules/translate/translate.actions';
 
 describe('SpokenToSignedComponent', () => {
   let component: SpokenToSignedComponent;
   let fixture: ComponentFixture<SpokenToSignedComponent>;
+  let store: Store;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -34,11 +36,21 @@ describe('SpokenToSignedComponent', () => {
     fixture = TestBed.createComponent(SpokenToSignedComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+
+    store = TestBed.inject(Store);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('text change should dispatch action', fakeAsync(() => {
+    const spy = spyOn(store, 'dispatch');
+    component.text.patchValue('test');
+    tick(500);
+
+    expect(spy).toHaveBeenCalledWith(new SetSpokenLanguageText('test'));
+  }));
 
   // TODO test state
   // it('empty text should set pose to null', fakeAsync(() => {
