4f022d9946794043c2fa947ab1a1a604fd3d8f32
Amit <amitmoryossef@gmail.com>
2021-08-19 16:08:37 +0300

    fix(sitemap): modify relative dir

--- tools/sitemap-generator.js
+++ tools/sitemap-generator.js
@@ -5,8 +5,10 @@ const fs = require("fs")
 const now = new Date();
 const urls = ['/'];
 
+const baseDir = __dirname + `${path.sep}..${path.sep}`;
+
 // Add language urls
-const langsDir = __dirname + `${path.sep}..${path.sep}src${path.sep}assets${path.sep}i18n`;
+const langsDir = `${baseDir}src${path.sep}assets${path.sep}i18n`;
 for (const file of fs.readdirSync(langsDir)) {
   const [lang] = file.split('.');
   urls.push(`/?lang=${lang}`)
@@ -15,7 +17,7 @@ for (const file of fs.readdirSync(langsDir)) {
 // writes sitemaps and index out to the destination you provide.
 simpleSitemapAndIndex({
   hostname: 'https://sign.mt',
-  destinationDir: '../dist/sign-language-playground/',
+  destinationDir: `${baseDir}dist/sign-language-playground/`,
   sourceData: urls.map(url => ({url, lastmod: now})),
   gzip: false
 }).then(() => process.exit(0)).catch(() => process.exit(1));
