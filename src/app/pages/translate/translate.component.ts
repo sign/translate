67e1fa2f9d8b98bcfb0821a233dc38f16a027163
Amit <amitmoryossef@gmail.com>
2021-08-19 21:58:28 +0300

    feat(i18n): add automatic translation script

--- src/app/pages/translate/translate.component.ts
+++ src/app/pages/translate/translate.component.ts
@@ -24,11 +24,12 @@ export class TranslateComponent extends BaseComponent implements OnInit {
   signedLanguages = ['us', 'fr', 'es', 'sy', 'by', 'bg', 'fl', 'hr', 'cz', 'dk', 'in', 'nz', 'gb', 'ee', 'fi', 'at', 'de', 'cy', 'gr', 'is',
     'isl', 'it', 'jp', 'lv', 'lt', 'ir', 'pl', 'br', 'pt', 'ro', 'ru', 'sk', 'ar', 'cl', 'cu', 'mx', 'se', 'tr', 'ua', 'pk'];
 
-  spokenLanguages = ['en', 'fr', 'es', 'af', 'sq', 'am', 'ar', 'hy', 'az', 'eu', 'be', 'bn', 'bs', 'bg', 'ca', 'ceb', 'zh-CN', 'zh-TW',
-    'co', 'hr', 'cs', 'da', 'nl', 'eo', 'et', 'fi', 'fy', 'gl', 'ka', 'de', 'el', 'gu', 'ht', 'ha', 'haw', 'he', 'hi', 'hmn', 'hu', 'is',
-    'ig', 'id', 'ga', 'it', 'ja', 'jw', 'kn', 'kk', 'km', 'ko', 'ku', 'ky', 'lo', 'la', 'lv', 'lt', 'lb', 'mk', 'mg', 'ms', 'ml', 'mt',
-    'mi', 'mr', 'mn', 'my', 'ne', 'no', 'ny', 'ps', 'fa', 'pl', 'pt', 'pa', 'ro', 'ru', 'sm', 'gd', 'sr', 'st', 'sn', 'sd', 'si', 'sk',
-    'sl', 'so', 'su', 'sw', 'sv', 'tl', 'tg', 'ta', 'te', 'th', 'tr', 'uk', 'ur', 'uz', 'vi', 'cy', 'xh', 'yi', 'yo', 'zu'];
+  spokenLanguages = ['en', 'fr', 'es', 'af', 'sq', 'am', 'ar', 'hy', 'az', 'eu', 'be', 'bn', 'bs', 'bg', 'ca', 'ceb', 'ny', 'zh-CN', 'co',
+    'hr', 'cs', 'da', 'nl', 'eo', 'et', 'tl', 'fi', 'fy', 'gl', 'ka', 'de', 'el', 'gu', 'ht', 'ha', 'haw', 'he', 'hi', 'hmn', 'hu', 'is',
+    'ig', 'id', 'ga', 'it', 'ja', 'jw', 'kn', 'kk', 'km', 'rw', 'ko', 'ku', 'ky', 'lo', 'la', 'lv', 'lt', 'lb', 'mk', 'mg', 'ms', 'ml',
+    'mt', 'mi', 'mr', 'mn', 'my', 'ne', 'no', 'or', 'ps', 'fa', 'pl', 'pt', 'pa', 'ro', 'ru', 'sm', 'gd', 'sr', 'st', 'sn', 'sd', 'si',
+    'sk', 'sl', 'so', 'su', 'sw', 'sv', 'tg', 'ta', 'tt', 'te', 'th', 'tr', 'tk', 'uk', 'ur', 'ug', 'uz', 'vi', 'cy', 'xh', 'yi', 'yo', 'zu'
+  ];
 
 
   constructor(private store: Store, private transloco: TranslocoService) {
---
+++ tools/i18n-fixer.js
@@ -0,0 +1,71 @@
+const fetch = require('node-fetch');
+const path = require("path")
+const fs = require('fs');
+
+const DEFAULT_LANGUAGE = 'en';
+
+const baseDir = __dirname + `${path.sep}..${path.sep}`;
+const langsDir = `${baseDir}src${path.sep}assets${path.sep}i18n`;
+
+// Load language files
+const languages = {};
+for (const file of fs.readdirSync(langsDir)) {
+  const [lang] = file.split('.');
+  const filePath = langsDir + path.sep + file;
+  languages[lang] = {
+    filePath,
+    content: JSON.parse(String(fs.readFileSync(filePath)))
+  };
+}
+
+async function translate(text, sourceLanguage, targetLanguage) {
+  console.log("translate", {text, sourceLanguage, targetLanguage})
+  const requestStr = `https://translate.googleapis.com/translate_a/single?client=gtx&sl=${sourceLanguage}&tl=${targetLanguage}&dt=t&q=${encodeURIComponent(text)}`;
+
+  const req = await fetch(requestStr);
+  const json = await req.json();
+  return json[0][0][0];
+}
+
+async function translateMissingEntries(source, target, targetLanguage) {
+  for (const [key, value] of Object.entries(source)) {
+    if (!(key in target)) {
+      if (typeof value === "string") {
+        target[key] = await translate(value, DEFAULT_LANGUAGE, targetLanguage);
+      } else {
+        target[key] = {};
+        await translateMissingEntries(value, target[key], targetLanguage);
+      }
+    } else {
+      if (typeof value !== "string") {
+        await translateMissingEntries(value, target[key], targetLanguage);
+      }
+    }
+  }
+}
+
+
+async function main() {
+  // Iterate files and translate missing entries
+  for (const [lang, {content, filePath}] of Object.entries(languages)) {
+    if (lang !== DEFAULT_LANGUAGE) {
+      await translateMissingEntries(languages[DEFAULT_LANGUAGE].content, content, lang);
+      fs.writeFileSync(filePath, JSON.stringify(content, null, 2) + '\n');
+    }
+  }
+}
+
+main().then(() => process.exit(0)).catch(() => process.exit(1));
+
+
+/** NOTE!
+ For spoken languages, go to Google Translate (https://translate.google.com/?hl=iw)
+ And get all their names:
+
+ const langs = {};
+ Array.from($0.querySelectorAll("div[data-language-code]")).forEach(n => {
+  langs[n.getAttribute("data-language-code")] = n.children[1].innerText.trim();
+ });
+ JSON.stringify(langs);
+ **/
+
