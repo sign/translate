b2d053e387ded30239537593bdb4dcb0687277df
amitmoryossef <amitmoryossef@gmail.com>
2021-12-25 21:56:08 +0100

    test(appearance): add import dependencies

--- src/app/pages/translate/appearance/appearance.component.spec.ts
+++ src/app/pages/translate/appearance/appearance.component.spec.ts
@@ -1,6 +1,13 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { AppearanceComponent } from './appearance.component';
+import {AppearanceComponent} from './appearance.component';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {NgxsModule} from '@ngxs/store';
+import {ngxsConfig} from '../../../core/modules/ngxs/ngxs.module';
+import {SettingsState} from '../../../modules/settings/settings.state';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {AppTranslocoModule} from '../../../core/modules/transloco/transloco.module';
 
 describe('AppearanceComponent', () => {
   let component: AppearanceComponent;
@@ -8,9 +15,15 @@ describe('AppearanceComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ AppearanceComponent ]
-    })
-    .compileComponents();
+      declarations: [AppearanceComponent],
+      imports: [
+        MatButtonModule,
+        MatIconModule,
+        MatToolbarModule,
+        AppTranslocoModule,
+        NgxsModule.forRoot([SettingsState], ngxsConfig)
+      ]
+    }).compileComponents();
   });
 
   beforeEach(() => {
--- src/app/pages/translate/input/button/button.component.spec.ts
+++ src/app/pages/translate/input/button/button.component.spec.ts
@@ -1,6 +1,12 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { TranslateInputButtonComponent } from './button.component';
+import {TranslateInputButtonComponent} from './button.component';
+import {NgxsModule} from '@ngxs/store';
+import {ngxsConfig} from '../../../../core/modules/ngxs/ngxs.module';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {TranslateState} from '../../../../modules/translate/translate.state';
+import {AppTranslocoModule} from '../../../../core/modules/transloco/transloco.module';
 
 describe('TranslateInputButtonComponent', () => {
   let component: TranslateInputButtonComponent;
@@ -8,9 +14,14 @@ describe('TranslateInputButtonComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ TranslateInputButtonComponent ]
-    })
-    .compileComponents();
+      declarations: [TranslateInputButtonComponent],
+      imports: [
+        MatButtonModule,
+        MatIconModule,
+        AppTranslocoModule,
+        NgxsModule.forRoot([TranslateState], ngxsConfig)
+      ]
+    }).compileComponents();
   });
 
   beforeEach(() => {
