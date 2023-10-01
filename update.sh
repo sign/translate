7d01505bff6ff19be1f9f25974db5145be3197c3
Amit Moryossef <amitmoryossef@gmail.com>
2023-10-01 15:33:44 +0200

    chore(): update dependencies

--- update.sh
+++ update.sh
@@ -6,4 +6,4 @@ npm run mobile:sync > /dev/null
 npx cap sync > /dev/null
 
 # Lint to fix wrongly formatted files
-prettier --write --ignore-unknown src > /dev/null
+#prettier --write --ignore-unknown src > /dev/null
