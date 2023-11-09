d3ed62cd6a81c62f17f8875d0e3ec0a28b285e47
Amit Moryossef <amitmoryossef@gmail.com>
2023-11-09 14:22:51 +0100

    chore(): update dependencies

--- update-dependencies.sh
+++ update-dependencies.sh
@@ -4,8 +4,6 @@ npm install
 
 # filesize: ngx-filesize 3 requires filesize >= 6.0.0 < 10.0.0 as a peer dependency
 
-# TODO @playwright/test 0.140 is buggy for us until updating typescript probably
-
 # Update server dependencies
 bun x npm-check-updates --cacheClear --cwd functions -u --reject node-fetch
 cd functions && bun install && cd ..
