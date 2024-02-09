7deb33e478211ff17591daef00e5b57ba220343b
Amit Moryossef <amitmoryossef@gmail.com>
2024-02-09 11:10:34 +0100

    chore(): update dependencies

--- update.sh
+++ update.sh
@@ -5,5 +5,9 @@ npm run capacitor:assets > /dev/null
 npm run mobile:sync > /dev/null
 npx cap sync > /dev/null
 
+cd ios/App && fastlane run update_fastlane && cd ../..
+cd android && fastlane run update_fastlane && cd ..
+
+
 # Lint to fix wrongly formatted files
 #prettier --write --ignore-unknown src > /dev/null
