c225c51ad7548e4ddf795ce2155c10da2d7ad1e7
Amit Moryossef <amitmoryossef@gmail.com>
2024-10-01 18:27:04 +0200

    feat(team): add tim!

--- src/app/pages/landing/about/about-team/about-team.component.ts
+++ src/app/pages/landing/about/about-team/about-team.component.ts
@@ -8,6 +8,12 @@ import {Component} from '@angular/core';
 export class AboutTeamComponent {
   teamMembers = [
     {
+      name: 'Timothy Rabozzi',
+      avatar: 'assets/promotional/about/team/timothy-rabozzi.jpg',
+      title: 'CEO',
+      bio: 'CEO with a background in LegalTech, campaigning, and business analysis. Holds a Master of Law with a focus on public law and innovation from the University of Bern.',
+    },
+    {
       name: 'Amit Moryossef',
       avatar: 'assets/promotional/about/team/amit-moryossef.jpg',
       title: 'CTO',
@@ -16,7 +22,7 @@ export class AboutTeamComponent {
     {
       name: 'Eliyahu Moryossef',
       avatar: 'assets/promotional/about/team/eliyahu-moryossef.jpg',
-      title: 'CMO, Acting CEO',
+      title: 'CMO',
       bio: 'Experienced sales and marketing manager with a B.A. in Public Administration, studying for a Master of Business Administration (MBA).',
     },
   ];
