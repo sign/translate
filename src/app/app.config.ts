dffcd9b2c5dd37cffa4670a469753b427258214a
Amit Moryossef <amitmoryossef@gmail.com>
2024-12-01 18:24:23 +0100

    chore(): update dependencies

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,4 @@
-import {ApplicationConfig, provideZoneChangeDetection} from '@angular/core';
+import {ApplicationConfig} from '@angular/core';
 import {provideRouter, RouteReuseStrategy} from '@angular/router';
 
 import {routes} from './app.routes';
