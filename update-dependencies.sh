00279c7be3eb23ca950b61f90a78c785d2059724
AmitMY <amitmoryossef@gmail.com>
2025-10-30 09:26:02 +0100

    chore(): update dependencies

--- update-dependencies.sh
+++ update-dependencies.sh
@@ -1,10 +1,9 @@
 # Update client dependencies
-bun x npm-check-updates --cacheClear -u --reject filesize,typescript,eslint,mermaid,@mediapipe/tasks-text
+bun x npm-check-updates --cacheClear -u --reject filesize,typescript,mermaid,@mediapipe/tasks-text
 npm install
 
 # filesize: ngx-filesize 3 requires filesize >= 6.0.0 < 10.0.0 as a peer dependency
 # typescript: The Angular Compiler requires TypeScript >=5.2.0 and <5.3.0
-# eslint: angular eslint requires eslint < 9.0.0
 # mermaid: mermaid 11.0.2 crashes tests
 
 # Update docs dependencies
